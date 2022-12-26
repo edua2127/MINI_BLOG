422cf7bf3c2ac78b1f3e2f8e8de573a888c3e966
Eduardo de Souza Magalhaes <84235466+edua2127@users.noreply.github.com>
2022-12-26 10:03:00 -0300

    css da home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,16 +8,19 @@ const Home = () => {
   const [query, setQuery] = useState("")
 
   const handleSubmit = (e) => {
-    
+    e.preventDefault()
   }
 
   return (
     <div>
       <h1>Veja os nossos posts mais recentes</h1>
-      <form>
-        <input type="text" placeholder="busque por tags.." />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="busque por tags.." value={query} onChange={(e)=> setQuery(e)} />
         <button className="btn btn-dark">Pesquisar</button>
       </form>
+      <div>
+        <h1> Posts </h1>
+      </div>
     </div>
   );
 };
